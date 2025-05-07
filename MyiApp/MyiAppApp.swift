aff77ec546474f84c649a083d3164d8267461f27
Tom Bum Su Choi <qjatn0545@naver.com>
2025-05-07 19:14:54 +0900

    initial commit

---
+++ MyiApp/MyiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyiAppApp.swift
+//  MyiApp
+//
+//  Created by 최범수 on 2025-05-07.
+//
+
+import SwiftUI
+
+@main
+struct MyiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
