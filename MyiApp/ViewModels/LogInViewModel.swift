3c0d6686ababbeb2fde0730bd5f20fc9fe1e31b4
Tom Bum Su Choi <qjatn0545@naver.com>
2025-05-07 20:38:02 +0900

    뼈대 구성

---
+++ MyiApp/ViewModels/LogInViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LogInViewModel.swift
+//  MyiApp
+//
+//  Created by 최범수 on 2025-05-07.
+//
+
+import Foundation
+
+class LogInViewModel: ObservableObject {
+    func appleLogin() {
+        
+    }
+    
+    func googleLogin() {
+        
+    }
+}
