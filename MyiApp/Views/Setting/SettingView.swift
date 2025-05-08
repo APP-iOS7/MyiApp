89550b38004c70bff263c085223375386f92b597
Tom Bum Su Choi <qjatn0545@naver.com>
2025-05-08 09:44:22 +0900

    홈, 노트, 기록, 설정, 통계 뷰모델 및 뷰 파일 추가

---
+++ MyiApp/Views/Setting/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  MyiApp
+//
+//  Created by 최범수 on 2025-05-08.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
---
+++ MyiApp/Views/Statistic/StatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticView.swift
+//  MyiApp
+//
+//  Created by 최범수 on 2025-05-08.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticView()
+}
