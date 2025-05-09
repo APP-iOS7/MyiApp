51b392ea03097c4965e6480c23a992d68e065aad
Tom Bum Su Choi <qjatn0545@naver.com>
2025-05-09 23:31:29 +0900

    TestLogInViewModel 및 TestLogInView 파일 추가

---
+++ MyiApp/ViewModels/LogIn/TestLogInViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TestLogInViewModel.swift
+//  MyiApp
+//
+//  Created by 최범수 on 2025-05-09.
+//
+
+import Foundation
+
+class TestLogInViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+}
