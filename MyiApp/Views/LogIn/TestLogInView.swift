787e68ff11e6f0be6e8177082809daba93bc48a4
Tom Bum Su Choi <qjatn0545@naver.com>
2025-05-10 19:11:11 +0900

    test Complete

--- MyiApp/Views/LogIn/TestLogInView.swift
+++ MyiApp/Views/LogIn/TestLogInView.swift
@@ -11,62 +11,23 @@ struct TestLogInView: View {
     @StateObject var viewModel: TestLogInViewModel = .init()
     
     var body: some View {
-        VStack(spacing: 20) {
-            // 로고 또는 앱 이름
-            Text("MyiApp")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.bottom, 30)
-            
-            // 이메일 입력 필드
-            TextField("이메일", text: $viewModel.email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-            
-            // 비밀번호 입력 필드
-            SecureField("비밀번호", text: $viewModel.password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-            
-            // 로그인 버튼
-            Button { viewModel.signIn() } label: {
-                Text("로그인")
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            .padding(.horizontal)
-            
-            // 회원가입
-            Button { viewModel.signUp() } label: {
-                Text("회원가입")
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            .padding(.horizontal)
-            
-            Button { viewModel.signInWithGoogle() } label: {
-                Text("구글 로그인")
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            .padding(.horizontal)
-            
-            if let errorMessage = viewModel.error {
-                Text(errorMessage)
-            }
-                
+        Button { viewModel.signInWithGoogle() } label: {
+            Text("구글 로그인")
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
         }
         .padding(.horizontal)
+        
+        if let errorMessage = viewModel.error {
+            Text(errorMessage)
+        }
+        
     }
+    
+    
 }
 
 
