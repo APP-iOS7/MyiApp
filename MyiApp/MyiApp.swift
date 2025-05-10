d938735fa59652234088543338432bd33742055f
Tom Bum Su Choi <qjatn0545@naver.com>
2025-05-10 19:11:11 +0900

    test Complete

--- MyiApp/MyiApp.swift
+++ MyiApp/MyiApp.swift
@@ -10,14 +10,12 @@ import FirebaseCore
 import GoogleSignIn
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         return true
     }
-    func application(_ app: UIApplication,
-                         open url: URL,
-                         options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
             return GIDSignIn.sharedInstance.handle(url)
         }
 }
