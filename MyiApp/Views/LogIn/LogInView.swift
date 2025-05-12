9f7c5f005905f2bb0155c95c6d96d2ecaceb57bb
Yung Hak Lee <celebi3@naver.com>
2025-05-12 10:14:07 +0900

    Feat: LoginView VStack

--- MyiApp/Views/LogIn/LogInView.swift
+++ MyiApp/Views/LogIn/LogInView.swift
@@ -12,71 +12,73 @@ struct LogInView: View {
     @StateObject var viewModel: LogInViewModel = .init()
     
     var body: some View {
-        Text("MyiApp")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .padding(.bottom, 30)
-        
-        // 이메일 입력 필드
-        TextField("이메일", text: $viewModel.email)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
+        VStack {
+            Text("MyiApp")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 30)
+            
+            // 이메일 입력 필드
+            TextField("이메일", text: $viewModel.email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            // 비밀번호 입력 필드
+            SecureField("비밀번호", text: $viewModel.password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            // 로그인 버튼
+            Button {
+                Task {
+                    do {
+                        try await viewModel.signIn()
+                    } catch {
+                        viewModel.error = error.localizedDescription
+                    }
+                }
+            } label: {
+                Text("로그인")
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
             .padding(.horizontal)
-        
-        // 비밀번호 입력 필드
-        SecureField("비밀번호", text: $viewModel.password)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            // 회원가입
+            Button {
+                Task {
+                    do {
+                        try await viewModel.signUp()
+                    } catch {
+                        viewModel.error = error.localizedDescription
+                    }
+                }
+            }  label: {
+                Text("회원가입")
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
             .padding(.horizontal)
-        
-        // 로그인 버튼
-        Button {
-            Task {
-                do {
-                    try await viewModel.signIn()
-                } catch {
-                    viewModel.error = error.localizedDescription
+            
+            // 구글 로그인 버튼
+            GoogleSignInButton(action: {
+                Task {
+                    do {
+                        try await viewModel.signInWithGoogle()
+                    } catch {
+                        viewModel.error = error.localizedDescription
+                    }
                 }
-            }
-        } label: {
-            Text("로그인")
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(10)
+            })
+            .frame(maxWidth: .infinity)
+            .padding()
         }
-        .padding(.horizontal)
-        
-        // 회원가입
-        Button {
-            Task {
-                do {
-                    try await viewModel.signUp()
-                } catch {
-                    viewModel.error = error.localizedDescription
-                }
-            }
-        }  label: {
-            Text("회원가입")
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(10)
-        }
-        .padding(.horizontal)
-        
-        // 구글 로그인 버튼
-        GoogleSignInButton(action: {
-            Task {
-                do {
-                    try await viewModel.signInWithGoogle()
-                } catch {
-                    viewModel.error = error.localizedDescription
-                }
-            }
-        })
-        .frame(maxWidth: .infinity)
-        .padding()
     }
 }
 
