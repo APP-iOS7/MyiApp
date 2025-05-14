7a2259a44b907609ab7755ed8e4a60554b781b6a
Yung Hak Lee <celebi3@naver.com>
2025-05-14 09:01:43 +0900

    Feat: 더보기 컴포넌트 추가
    - 프로필 탭, 아이 정보 탭, 개인정보 처리방침 탭, 이용약관 탭, 알림설정 탭 추가
    - 로그아웃 버튼 변경 및 Alert 표시

---
+++ MyiApp/ViewModels/Setting/AccountSettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfileViewModel.swift
+//  MyiApp
+//
+//  Created by Yung Hak Lee on 5/13/25.
+//
+
+import Foundation
---
+++ MyiApp/Views/Setting/AccountSettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  AccountSettingsView.swift
+//  MyiApp
+//
+//  Created by Yung Hak Lee on 5/13/25.
+//
+
+import SwiftUI
+
+struct AccountSettingsView: View {
+    var body: some View {
+        Text("사용자 프로필")
+    }
+}
