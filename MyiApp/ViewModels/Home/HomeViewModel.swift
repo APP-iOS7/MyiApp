1c8531047c9c68b994b6e94e7b02871995e805d0
Tom Bum Su Choi <qjatn0545@naver.com>
2025-05-14 20:55:36 +0900

    feat: record 추가 구현

--- MyiApp/ViewModels/Home/HomeViewModel.swift
+++ MyiApp/ViewModels/Home/HomeViewModel.swift
@@ -7,20 +7,33 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 class HomeViewModel: ObservableObject {
-    @Published var selectedBaby: Baby?
-    @Published var records: [Record] = Record.mockRecords
+    @Published var baby: Baby = CaregiverManager.shared.selectedBaby!
     @Published var selectedDate: Date = Date()
     @Published var selectedCategory: GridItemCategory?
     @Published var isFlipped = false
-//    private let db = DatabaseService.shared
+    private var cancellables = Set<AnyCancellable>()
+    private let caregiverManager = CaregiverManager.shared
+    var displayName: String {
+        baby.name
+    }
+    var displayGender: String {
+        baby.gender.rawValue == 1 ? "남자" : "여자"
+    }
+    var displayBirthDate: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        return formatter.string(from: baby.birthDate)
+    }
+    var displayMonthDay: String {
+        let diff = Calendar.current.dateComponents([.month, .day], from: baby.birthDate, to: Date())
+        return "\(diff.month ?? 0)개월 \(diff.day ?? 0)일"
+    }
+    var displayDayCount: String {
+        let days = Calendar.current.dateComponents([.day], from: baby.birthDate, to: Date()).day ?? 0
+        return "\(days + 1)일"
+    }
     
-    func loadBabyInfo() { }
-    
-    func loadRecords(for date: Date) { }
-    
-    func addRecord(_ record: Record) { }
-    
-    func deleteRecord(_ record: Record) { }
 }
