d78c20fd0b2e4d170eaa8ddbcd7e7e7e62cef0b9
tomchoi <qjatn0545@naver.com>
2026-03-10 03:08:41 +0900

    chore(Tuist): Xcode 버전 제한 제거, Swift 6.2로 변경

--- Tuist.swift
+++ Tuist.swift
@@ -1,9 +1,8 @@
 import ProjectDescription
 
 let tuist = Tuist(
-    compatibleXcodeVersions: .upToNextMajor("16.4"),
     fullHandle: "qjatn0545/MyiApp",
-    swiftVersion: "6.2.4",
+    swiftVersion: "6.2",
     generationOptions: .options(
         enableCaching: true,
         registryEnabled: true,
