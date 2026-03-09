aadf3e12fcf5a87bb5bdf75beb85dbd66a4bfe6b
tomchoi <qjatn0545@naver.com>
2026-03-09 19:58:14 +0900

    chore(DesignSystem): DesignSystem 모듈 추가

---
+++ DesignSystem/Project.swift
@@ -0,0 +1,16 @@
+import ProjectDescription
+
+let project = Project(
+    name: "DesignSystem",
+    targets: [
+        .target(
+            name: "DesignSystem",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.myiapp.DesignSystem",
+            infoPlist: .default,
+            sources: ["Sources/**"],
+            resources: ["Resources/**"]
+        ),
+    ]
+)
---
+++ DesignSystem/Sources/DesignSystem.swift
@@ -0,0 +1,3 @@
+import SwiftUI
+
+// DesignSystem: 디자인 토큰, 공용 UI 컴포넌트
