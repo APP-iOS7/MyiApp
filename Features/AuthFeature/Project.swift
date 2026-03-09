a362d3571f1febe474399e6f6eab2dc5cde2db90
tomchoi <qjatn0545@naver.com>
2026-03-09 19:58:17 +0900

    chore(Auth): AuthFeature 모듈 추가

---
+++ Features/AuthFeature/Project.swift
@@ -0,0 +1,28 @@
+import ProjectDescription
+
+let project = Project(
+    name: "AuthFeature",
+    targets: [
+        .target(
+            name: "AuthFeatureInterface",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.myiapp.AuthFeatureInterface",
+            infoPlist: .default,
+            sources: ["Sources/AuthFeatureInterface/**"],
+            dependencies: [.project(target: "Domain", path: "../../Domain")]
+        ),
+        .target(
+            name: "AuthFeature",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.myiapp.AuthFeature",
+            infoPlist: .default,
+            sources: ["Sources/AuthFeature/**"],
+            dependencies: [
+                .target(name: "AuthFeatureInterface"),
+                .project(target: "DesignSystem", path: "../../DesignSystem"),
+            ]
+        ),
+    ]
+)
