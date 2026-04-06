1a4f9e6512ea0d791a2977d7218ae619c9fdde27
TomBumSuChoi <qjatn0545@naver.com>
2026-04-06 14:52:54 +0900

    fix: AuthProvider enum제거 및 Session의 provider 타입 변경

--- MyIApp/Sources/Domain/Entities/Session.swift
+++ MyIApp/Sources/Domain/Entities/Session.swift
@@ -2,5 +2,5 @@ import Foundation
 
 struct Session: Equatable {
     let userID: String
-    let provider: AuthProvider
+    let provider: OAuthProvider
 }
--- MyIApp/Sources/Domain/Enums/AuthProvider.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-enum AuthProvider: String {
-    case firebase
-    case supabase
-}
