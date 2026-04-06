0bcd89c3429b84ee25f557a7e3f6331cabbdf2c5
TomBumSuChoi <qjatn0545@naver.com>
2026-04-06 17:48:20 +0900

    feat: Firebase 설정 추가

--- MyIApp/Sources/App/MyiApp.swift
+++ MyIApp/Sources/App/MyiApp.swift
@@ -1,10 +1,15 @@
 import ComposableArchitecture
+import Firebase
 import SwiftUI
 
 @main
 struct MyiApp: App {
     let store = Store(initialState: AppFeature.State()) {
         AppFeature()
+    }
+
+    init() {
+        FirebaseApp.configure()
     }
 
     var body: some Scene {
