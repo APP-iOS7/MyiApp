ebfa7190336e63388b9656c1bce704b832c94e7b
TomBumSuChoi <qjatn0545@naver.com>
2026-04-06 12:38:13 +0900

    feat: TCA의존성 추가

--- MyIApp/Project.swift
+++ MyIApp/Project.swift
@@ -14,7 +14,9 @@ let project = Project(
             ]),
             sources: ["Sources/**"],
             resources: ["Resources/**"],
-            dependencies: []
+            dependencies: [
+                .external(name: "ComposableArchitecture"),
+            ]
         ),
     ]
 )
