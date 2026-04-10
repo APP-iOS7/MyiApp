b6de34dcb74018f75b1f6b055b017c12920bc172
TomBumSuChoi <qjatn0545@naver.com>
2026-04-10 14:51:17 +0900

    feat: Primary Button Style 작성

--- MyIApp/Sources/Core/DesignSystem/Tokens/AppSizing.swift
+++ MyIApp/Sources/Core/DesignSystem/Tokens/AppSizing.swift
@@ -5,5 +5,7 @@ extension DesignSystem {
     public enum Sizing {
         /// 메인 일러스트레이션 크기 (기본 431)
         public static let illustrationLarge: CGFloat = 431
+        /// 기본 버튼 높이 (기본 56)
+        public static let defaultButtonHeight: CGFloat = 56
     }
 }
