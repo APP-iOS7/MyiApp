b5941175134543c8dc6ff884c55c17e5099043ee
TomBumSuChoi <qjatn0545@naver.com>
2026-04-12 12:08:56 +0900

    feat: BabyClient에 fetchBabies 메서드 추가 (아이 정보 조회)

--- MyIApp/Sources/Domain/Interfaces/BabyClient.swift
+++ MyIApp/Sources/Domain/Interfaces/BabyClient.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct BabyClient: Sendable {
+    public var fetchBabies: @Sendable () async throws -> [Baby]
     public var registerNewBaby: @Sendable (NewBabyRequest) async throws -> Baby
     public var registerExistingBaby: @Sendable (String) async throws -> Baby
 }
