a86d712541a3152ffb3ed40e1b79ed7b93ab7097
TomBumSuChoi <qjatn0545@naver.com>
2026-04-26 18:56:10 +0900

    feat: Clients 패키지에 Firestore 및 Storage 의존성 추가
    - Baby/Record 등 도메인 데이터 영속화를 위한 FirebaseFirestore 추가
    - 프로필 이미지 등 바이너리 자산 저장용 FirebaseStorage 추가

--- Packages/Clients/Package.swift
+++ Packages/Clients/Package.swift
@@ -29,6 +29,8 @@ let package = Package(
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
                 .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
                 .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
                 .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
             ]
