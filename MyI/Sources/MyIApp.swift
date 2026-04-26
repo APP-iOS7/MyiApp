0633ea76d2ee5b59affea368993f424a895fd64e
TomBumSuChoi <qjatn0545@naver.com>
2026-04-27 01:59:57 +0900

    refactor: 앱 진입 라이프사이클을 AppFeature의 AppScene으로 이동
    - AppScene 신규: store 생성, AppBootstrap.configure(), WindowGroup, onOpenURL을 Scene 단위로 캡슐화
    - MyIApp은 AppScene만 합성하는 6줄짜리 진입점으로 단순화
    - App 타겟에서 Clients/ComposableArchitecture 직접 import 제거 (AppFeature를 통해 간접 의존)

--- MyI/Sources/MyIApp.swift
+++ MyI/Sources/MyIApp.swift
@@ -1,25 +1,7 @@
 import AppFeature
-import Clients
-import ComposableArchitecture
 import SwiftUI
 
 @main
 struct MyIApp: App {
-    let store: StoreOf<AppFeature>
-
-    init() {
-        AppBootstrap.configure()
-        store = Store(initialState: AppFeature.State()) {
-            AppFeature()
-        }
-    }
-
-    var body: some Scene {
-        WindowGroup {
-            AppView(store: store)
-                .onOpenURL { url in
-                    _ = AppBootstrap.handle(url: url)
-                }
-        }
-    }
+    var body: some Scene { AppScene() }
 }
