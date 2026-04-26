1fb47e2554599e0d2487e86d1a4f525f0c6864f6
TomBumSuChoi <qjatn0545@naver.com>
2026-04-26 17:55:54 +0900

    feat: Clients 패키지에 Domain 및 TCA 의존성 추가

--- Packages/Clients/Package.swift
+++ Packages/Clients/Package.swift
@@ -7,25 +7,30 @@ let package = Package(
     name: "Clients",
     platforms: [
         .iOS(.v17),
-        .macOS(.v14),
+        .macOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Clients",
             targets: ["Clients"]
-        ),
+        )
+    ],
+    dependencies: [
+        .package(path: "../Domain"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Clients"
+            name: "Clients",
+            dependencies: [
+                .product(name: "Domain", package: "Domain"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
         ),
         .testTarget(
             name: "ClientsTests",
             dependencies: ["Clients"]
-        ),
+        )
     ],
     swiftLanguageModes: [.v6]
 )
