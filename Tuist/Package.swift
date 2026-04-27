4a4737a9928586b33b854fe993148c6d65aa3dfb
TomBumSuChoi <qjatn0545@naver.com>
2026-04-27 16:49:24 +0900

    chore(Tuist): 외부 의존성을 Tuist/Package.swift로 이전

---
+++ Tuist/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+#if TUIST
+import struct ProjectDescription.PackageSettings
+
+let packageSettings = PackageSettings(
+    productTypes: [
+        "ComposableArchitecture": .staticFramework
+    ]
+)
+#endif
+
+let package = Package(
+    name: "ExternalDependencies",
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            .upToNextMinor(from: "1.25.5")
+        ),
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            .upToNextMinor(from: "12.12.1")
+        ),
+        .package(
+            url: "https://github.com/google/GoogleSignIn-iOS",
+            .upToNextMinor(from: "9.1.0")
+        )
+    ]
+)
