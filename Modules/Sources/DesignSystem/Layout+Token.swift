320d90202f7819cbe92f3df8f6590fc2c1fc7199
TomBumSuChoi <qjatn0545@naver.com>
2026-04-27 15:15:30 +0900

    feat(DesignSystem): 레이아웃·타이포 토큰 추가, 소셜 버튼을 토큰 기반으로 갱신

---
+++ Modules/Sources/DesignSystem/Layout+Token.swift
@@ -0,0 +1,25 @@
+import CoreFoundation
+
+public enum Spacing {
+    public static let xs: CGFloat = 4
+    public static let s: CGFloat = 8
+    public static let m: CGFloat = 16
+    public static let l: CGFloat = 24
+    public static let xl: CGFloat = 32
+}
+
+public enum Radius {
+    public static let s: CGFloat = 5
+    public static let m: CGFloat = 8
+    public static let l: CGFloat = 12
+}
+
+public enum ButtonSize {
+    public static let height: CGFloat = 50
+}
+
+public enum IconSize {
+    public static let s: CGFloat = 18
+    public static let m: CGFloat = 24
+    public static let l: CGFloat = 32
+}
