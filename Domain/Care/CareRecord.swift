a2ea4039316cf926b0e9f2792fb546f3dee4cfe8
TomBumSuChoi <qjatn0545@naver.com>
2026-04-28 12:22:51 +0900

    refactor(Domain): CareRecord.mocks를 #if DEBUG로 감싸고 샘플 6개로 축소
    릴리스 빌드에 mock 포함되지 않도록 가드.

--- Domain/Care/CareRecord.swift
+++ Domain/Care/CareRecord.swift
@@ -18,3 +18,20 @@ public struct CareRecord: Identifiable, Hashable, Sendable, Codable {
         self.content = content
     }
 }
+
+#if DEBUG
+extension CareRecord {
+    public static let mocks: [CareRecord] = {
+        let now = Date()
+        let cal = Calendar.current
+        return [
+            CareRecord(createdAt: cal.date(byAdding: .hour, value: -5, to: now)!, event: .formula(ml: 120)),
+            CareRecord(createdAt: cal.date(byAdding: .hour, value: -4, to: now)!, event: .breastfeeding(leftMinutes: 10, rightMinutes: 8)),
+            CareRecord(createdAt: cal.date(byAdding: .hour, value: -3, to: now)!, event: .pee),
+            CareRecord(createdAt: cal.date(byAdding: .hour, value: -2, to: now)!, event: .sleep(start: cal.date(byAdding: .hour, value: -4, to: now)!, end: cal.date(byAdding: .hour, value: -2, to: now)!)),
+            CareRecord(createdAt: cal.date(byAdding: .hour, value: -1, to: now)!, event: .temperature(celsius: 36.5)),
+            CareRecord(createdAt: now, event: .bath, content: "목욕 완료"),
+        ]
+    }()
+}
+#endif
