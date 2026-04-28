89326f9c38a09e9321dd46c953cd8d3cecbca3be
TomBumSuChoi <qjatn0545@naver.com>
2026-04-28 09:39:00 +0900

    feat(Domain): CareRecord/CareEvent 도메인 모델 추가
    - CareEvent: 카테고리+페이로드를 enum with associated values로 통합
    - CareEvent.Category: feeding/potty/sleep/bath/snack/vital/medical 분류
    - CareRecord: id/createdAt/event/content 4필드로 단순화

---
+++ Domain/Care/CareEvent.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+public enum CareEvent: Hashable, Sendable, Codable {
+    case formula(ml: Int)
+    case babyFood(ml: Int)
+    case pumpedMilk(ml: Int)
+    case breastfeeding(leftMinutes: Int, rightMinutes: Int)
+    case pee
+    case poop
+    case pottyAll
+    case sleep(start: Date, end: Date)
+    case bath
+    case snack
+    case temperature(celsius: Double)
+    case medicine
+    case clinic
+}
+
+extension CareEvent {
+    public enum Category: String, CaseIterable, Hashable, Sendable {
+        case feeding        // formula/babyFood/pumpedMilk/breastfeeding
+        case potty          // pee/poop/pottyAll
+        case sleep
+        case bath
+        case snack
+        case vital          // temperature
+        case medical        // medicine/clinic
+    }
+
+    public var category: Category {
+        switch self {
+        case .formula, .babyFood, .pumpedMilk, .breastfeeding:  .feeding
+        case .pee, .poop, .pottyAll:                            .potty
+        case .sleep:                                            .sleep
+        case .bath:                                             .bath
+        case .snack:                                            .snack
+        case .temperature:                                      .vital
+        case .medicine, .clinic:                                .medical
+        }
+    }
+}
