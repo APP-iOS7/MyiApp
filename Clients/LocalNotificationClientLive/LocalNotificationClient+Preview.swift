28a823516e8ef5819bcf8a6d299842311bc89657
TomBumSuChoi <qjatn0545@naver.com>
2026-04-30 13:05:04 +0900

    feat(Notification): authorizationStatus + LocalNotificationAuthorization

--- Clients/LocalNotificationClientLive/LocalNotificationClient+Preview.swift
+++ Clients/LocalNotificationClientLive/LocalNotificationClient+Preview.swift
@@ -4,6 +4,7 @@ import Foundation
 
 extension LocalNotificationClient {
     public static let previewValue = Self(
+        authorizationStatus: { .authorized },
         requestPermission: { true },
         schedule: { _ in },
         cancel: { _ in }
--- Domain/Notification/LocalNotificationClient.swift
+++ Domain/Notification/LocalNotificationClient.swift
@@ -1,15 +1,18 @@
 import Foundation
 
 public struct LocalNotificationClient: Sendable {
+    public var authorizationStatus: @Sendable () async -> LocalNotificationAuthorization
     public var requestPermission: @Sendable () async -> Bool
     public var schedule: @Sendable (LocalNotificationRequest) async throws(LocalNotificationError) -> Void
     public var cancel: @Sendable (UUID) async -> Void
 
     public init(
+        authorizationStatus: @escaping @Sendable () async -> LocalNotificationAuthorization,
         requestPermission: @escaping @Sendable () async -> Bool,
         schedule: @escaping @Sendable (LocalNotificationRequest) async throws(LocalNotificationError) -> Void,
         cancel: @escaping @Sendable (UUID) async -> Void
     ) {
+        self.authorizationStatus = authorizationStatus
         self.requestPermission = requestPermission
         self.schedule = schedule
         self.cancel = cancel
