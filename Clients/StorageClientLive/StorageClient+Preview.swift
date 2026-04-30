502f435918441146cdafc30407cbe2263858d13b
TomBumSuChoi <qjatn0545@naver.com>
2026-04-30 14:34:10 +0900

    feat(Storage): URL 기반 uploadDiaryPhotoFile 추가

--- Clients/StorageClientLive/StorageClient+Preview.swift
+++ Clients/StorageClientLive/StorageClient+Preview.swift
@@ -7,6 +7,9 @@ extension StorageClient {
         uploadDiaryPhoto: { _, _, _ in
             URL(string: "https://picsum.photos/200") ?? URL(filePath: "/")
         },
+        uploadDiaryPhotoFile: { _, _, _ in
+            URL(string: "https://picsum.photos/200") ?? URL(filePath: "/")
+        },
         deleteDiaryPhoto: { _ in }
     )
 }
