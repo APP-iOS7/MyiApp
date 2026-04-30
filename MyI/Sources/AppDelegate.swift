2979a4f102d03b4093a6ae03996ecf992118024e
TomBumSuChoi <qjatn0545@naver.com>
2026-04-30 21:15:12 +0900

    refactor(Bootstrap): NotificationCoordinator + APNs handlers 흡수, AppDelegate 단순화

---
+++ MyI/Sources/AppDelegate.swift
@@ -0,0 +1,37 @@
+import Clients
+import DesignSystem
+import UIKit
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        AppBootstrap.configure()
+        DesignSystemFontFamily.registerAllCustomFonts()
+        application.registerForRemoteNotifications()
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        AppBootstrap.handleAPNsToken(deviceToken)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+        AppBootstrap.handleAPNsFailure(error)
+    }
+
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        AppBootstrap.handle(url: url)
+    }
+}
