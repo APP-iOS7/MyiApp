dd8b8c5a2c030b0b4b0d44f744dac14a84d7df91
TomBumSuChoi <qjatn0545@naver.com>
2026-05-01 22:51:17 +0900

    feat(Domain/Gender): displayName 한국어 표기 추가
    남자/여자 매핑이 view 단에 흩어져 있어 도메인으로 끌어올림.

--- Domain/Baby/Gender.swift
+++ Domain/Baby/Gender.swift
@@ -1,4 +1,11 @@
 public enum Gender: String, CaseIterable, Sendable, Codable {
     case male
     case female
+
+    public var displayName: String {
+        switch self {
+        case .male: "남자"
+        case .female: "여자"
+        }
+    }
 }
